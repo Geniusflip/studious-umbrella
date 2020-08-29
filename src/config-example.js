083a6b514b485ff6e2ecb86178ff6e31fd368edd
Ryan Herford <ryan.herford@servian.com>
2020-08-29 22:23:52 +1000

    Provide config example

---
+++ src/config-example.js
@@ -0,0 +1,18 @@
+const config = {
+    "firebase": {
+        "apiKey": "_",
+        "authDomain": "_",
+        "databaseURL": "_",
+        "projectId": "_",
+        "storageBucket": "_",
+        "messagingSenderId": "_",
+        "appId": "_",
+        "measurementId": "_"
+    },
+    "allowedUsers": ["example@example.com"],
+    "internalUrlPrefix": window.location.origin,
+    "saveUrlPrefix": "http://internal",
+}  
+export {
+   config,
+} 
